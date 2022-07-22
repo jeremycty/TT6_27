45ef0b10152bb2e421898e9b4379f877418294f4
Jeremy <jeremy.chng@u.nus.edu>
2022-07-22 14:06:49 +0800

    Populated currencyaccount

--- main.py
+++ main.py
@@ -34,14 +34,14 @@ class ExchangeRate(db.Model):
         return dictionary
 
 ##create Currency TABLE
-class currency(db.Model):
+class Currency(db.Model):
     __tablename__ = "currency"
     id = db.Column(db.Integer, primary_key=True)
     currency = db.Column(db.String(100), nullable=False)
-    amount = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Integer, nullable=True)
 
 ##create Transaction TABLE
-class transaction(db.Model):
+class Transaction(db.Model):
     __tablename__ = "transaction"
     id = db.Column(db.Integer, primary_key=True)
     debit_currency = db.Column(db.String(100), nullable=False)
@@ -57,7 +57,161 @@ class transaction(db.Model):
     def __repr__(self):
         return f'{self.debit_currency} - {self.debit_amount}'
   
-# db.create_all()
+db.create_all()
+currencyaccountdata=[
+  {
+    "id": 1,
+    "wallet_id": 1,
+    "currency": "SGD",
+    "amount": 4294.50
+  },
+  {
+    "id": 2,
+    "wallet_id": 1,
+    "currency": "CAD",
+    "amount": 5687.65
+  },
+  {
+    "id": 3,
+    "wallet_id": 1,
+    "currency": "CNH",
+    "amount": 6063.14
+  },
+  {
+    "id": 4,
+    "wallet_id": 1,
+    "currency": "EUR",
+    "amount": 8089.82
+  },
+  {
+    "id": 5,
+    "wallet_id": 1,
+    "currency": "HKD",
+    "amount": 7862.36
+  },
+  {
+    "id": 6,
+    "wallet_id": 1,
+    "currency": "JPY",
+    "amount": 5759.15
+  },
+  {
+    "id": 7,
+    "wallet_id": 1,
+    "currency": "NZD",
+    "amount": 6943.26
+  },
+  {
+    "id": 8,
+    "wallet_id": 1,
+    "currency": "NOK",
+    "amount": 4038.10
+  },
+  {
+    "id": 9,
+    "wallet_id": 1,
+    "currency": "GBP",
+    "amount": 8287.33
+  },
+  {
+    "id": 10,
+    "wallet_id": 1,
+    "currency": "SEK",
+    "amount": 5126.40
+  },
+  {
+    "id": 11,
+    "wallet_id": 1,
+    "currency": "THB",
+    "amount": 147.62
+  },
+  {
+    "id": 12,
+    "wallet_id": 1,
+    "currency": "USD",
+    "amount": 7331.77
+  },
+  {
+    "id": 13,
+    "wallet_id": 2,
+    "currency": "SGD",
+    "amount": 485.19
+  },
+  {
+    "id": 14,
+    "wallet_id": 2,
+    "currency": "CAD",
+    "amount": 2634.58
+  },
+  {
+    "id": 15,
+    "wallet_id": 2,
+    "currency": "CNH",
+    "amount": 3893.29
+  },
+  {
+    "id": 16,
+    "wallet_id": 2,
+    "currency": "EUR",
+    "amount": 3887.15
+  },
+  {
+    "id": 17,
+    "wallet_id": 2,
+    "currency": "HKD",
+    "amount": 4065.34
+  },
+  {
+    "id": 18,
+    "wallet_id": 2,
+    "currency": "JPY",
+    "amount": 1702.47
+  },
+  {
+    "id": 19,
+    "wallet_id": 2,
+    "currency": "NZD",
+    "amount": 3299.38
+  },
+  {
+    "id": 20,
+    "wallet_id": 2,
+    "currency": "NOK",
+    "amount": 7681.32
+  },
+  {
+    "id": 21,
+    "wallet_id": 2,
+    "currency": "GBP",
+    "amount": 3720.37
+  },
+  {
+    "id": 22,
+    "wallet_id": 2,
+    "currency": "SEK",
+    "amount": 4511.50
+  },
+  {
+    "id": 23,
+    "wallet_id": 2,
+    "currency": "THB",
+    "amount": 6216.60
+  },
+  {
+    "id": 24,
+    "wallet_id": 2,
+    "currency": "USD",
+    "amount": 9103.66
+  }
+]
+for account in currencyaccountdata:
+    newaccount=Currency(currency=account["currency"],
+                amount=account["amount"])
+    db.session.add(newaccount)
+    db.session.commit()
+
+
+
 
 ### INITIALIZE LOGIN
 login_manager = LoginManager()
@@ -66,12 +220,12 @@ login_manager.init_app(app)
 def load_user(user_id):
     return User.query.get(user_id)
 
-
+#home
 @app.route('/')
 def home():
     return render_template("index.html")
 
-
+#login
 @app.route('/login', methods=["POST"])
 def login():
     if request.method=="POST":
@@ -81,22 +235,23 @@ def login():
 ######Check if user is registered in database
         account=User.query.filter_by(email=inputemail).first()
         if account is None:
-            flash('User does not exist')
+            return render_template("login.html", message='User does not exist')
 ######Check if password is correct
         else:
-            if account.password== inputpw:
-                flash('Incorrect Password')
+            if account.password != inputpw:
+                return render_template("login.html", message='Incorrect Password')
             else:
                 login_user(account)
-                flash('Logged in successfully.')
-                return redirect(url_for('home', current_user=current_user))
+                return redirect(url_for('home', message='Logged in successfully.'))
     return render_template("login.html")
 
+#logout
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
+#exchange rates
 @app.route('/exchange-rates')
 def exchange_rates():
     exchangerates=ExchangeRate.query.all()
