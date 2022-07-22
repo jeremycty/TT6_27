5d7e5cadced93508974abfd38783dcd9a822d8c2
Sha <shafiq.shamuri@gmail.com>
2022-07-22 12:47:37 +0800

    login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+// import { Routes, Route } from 'react-router-dom';
 import './css/App.css';
-import Header from "./components/Header"
-import Home from "./page/Home"
-import Secondpage from "./page/Secondpage"
+// import Header from './components/Header';
+// import Home from './page/Home';
+// import Secondpage from './page/Secondpage';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  const userTest = {
+    email: 'user@user.com',
+    password: 'user',
+  };
+  // test user hardcode details
+
+  const [user, setUser] = useState({ name: '', email: '' });
+  const [error, setError] = useState('');
+
+  const Login = (details) => {
+    console.log(details);
+
+    if (
+      details.email === userTest.email &&
+      details.password === userTest.password
+    )
+      console.log('logged in');
+  };
+
+  const Logout = () => {
+    console.log('logout');
+  };
+
   return (
     <div className="App">
-      <Header/> 
-
-      {/* Routes for varous pages componenets */}
-      <Routes> 
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/second' element={<Secondpage/>}></Route>
-      </Routes> 
-
+      {user.email !== '' ? (
+        <div className="welcome">
+          <h2>
+            welcome, <span>{user.name}</span>
+          </h2>
+          <button>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
